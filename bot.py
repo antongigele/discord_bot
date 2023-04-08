2200ceba421fc8f30e557e2756ad3cfaa55c51f6
Anton Gigele <53263067+antongigele@users.noreply.github.com>
2023-04-08 23:35:49 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    string = 'Hello'
+
+    if message.content == string:
+        response = "Denk daran deine Affirmationen zu machen."
+        await message.channel.send(response)
+
+    print(message.content)
+
+client.run(TOKEN)
